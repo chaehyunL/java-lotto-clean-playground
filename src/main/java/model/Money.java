1dffec5c26caacf16f2e1fb910742ddbbbf86cb9
chaehyunL <chaehyun1012@gmail.com>
2026-03-30 06:34:38 +0900

    feat:Money.java 생성

---
+++ src/main/java/model/Money.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.Map;
+
+public class Money {
+    public final int money;
+    public final int amount;
+
+    public Money(int money) {
+        this.money = money;
+        this.amount = getAmount();
+    }
+
+    public int getAmount() {
+        int count;
+        count = money / 1000;
+        return count;
+    }
+
+    public int getProfit(Map<Integer,Integer>status){
+        int total=0;
+        total=status.get(3)*5000+
+                status.get(4)*50000+
+                status.get(5)*1500000+
+                status.get(6)*2000000000;
+        return total;
+    }
+
+    public double RoR(int profit) {
+        double rate = (double) profit / money;
+        return rate;
+    }
+}
