9fa3f6a923c0070f543125646ebaea9c0904d07a
chaehyunL <chaehyun1012@gmail.com>
2026-03-31 10:54:34 +0900

    refector: controller 메서드 분리

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -11,17 +11,39 @@ import java.util.Map;
 
 public class Controller {
     public void run() {
+        Inputview inputview=new Inputview();
+
+        Money money=payMoney();
+
+        LottoTickets lottoTickets =buyTickets(money);
+
+        showTickets(money,lottoTickets);
+
+        showStatus(lottoTickets,money);
+
+
+    }
+    private Money payMoney(){
         Inputview inputview = new Inputview();
         int getMoney = inputview.getMoney();
-
         Money money = new Money(getMoney);
+        return money;
+    }
+    private LottoTickets buyTickets(Money money){
         LottoTickets lottoTickets = new LottoTickets(money.getAmount());
         lottoTickets.buyTickets();
+    }
+
+    private void showTickets(Money money,LottoTickets lottoTickets){
         Outputview outputview = new Outputview();
         outputview.printMyTickets(money.getAmount(), lottoTickets);
+    }
 
+    private void showStatus(LottoTickets lottoTickets,Money money){
+        Inputview inputview=new Inputview();
         List<Integer> Resultnumber = inputview.getResult();
 
+        Outputview outputview=new Outputview();
         outputview.printHead();
         LottoResult lottoResult = new LottoResult();
         lottoResult.calculate(lottoTickets.getTickets(), Resultnumber);
