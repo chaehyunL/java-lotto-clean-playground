3d0dd312a9ca7bc4b85f3c05a8ab0934d8c9ce59
chaehyunL <chaehyun1012@gmail.com>
2026-04-02 10:00:11 +0900

    refector: 매직 넘버 상수 변경

--- src/main/java/model/RandomNumberGenerator.java
+++ src/main/java/model/RandomNumberGenerator.java
@@ -5,10 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class RandomNumberGenerator {
+    private static final int MinlottoNumber = 1;
+    private static final int MaxlottoNumber = 45;
     public List<Integer> getRandomNumberList() {
         List<Integer> pool = new ArrayList<>();
-        int MinlottoNumber = 1;
-        int MaxlottoNumber = 45;
+
         for (int i = MinlottoNumber; i <= MaxlottoNumber; i++) {
             pool.add(i);
         }
