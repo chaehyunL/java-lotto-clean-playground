d95176dc07cb0b392d583b13876629b31eaf0564
chaehyunL <chaehyun1012@gmail.com>
2026-04-04 16:29:28 +0900

    test: 메서드,객체 변경에 따른 test 내용 변경

--- src/test/java/TestLottoTicket.java
+++ src/test/java/TestLottoTicket.java
@@ -1,36 +1,48 @@
 import model.LottoTicket;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class TestLottoTicket {
-    @Test
-    @DisplayName("6개의 숫자가 출력되는가")
-    public void get6numbers() {
-        //Given: 로또 티켓 한개 생성
-        LottoTicket lottoTicket = new LottoTicket();
-        //When:로또 한개를 만들고 숫자를 부여
-        lottoTicket.makeLottoTicket();
-        List<Integer> ticket = lottoTicket.getLottoNumbers();
-        //Then:로또 1장당 6개의 숫자를 가짐
-        assertEquals(ticket.size(), 6);
-    }
-
     @Test
     @DisplayName("복권이 오름차순으로 정렬되어있는가")
     public void getnumberup() {
-        //Given:로또 티켓 생성
-        LottoTicket lottoTicket = new LottoTicket();
-        //When:로또 티켓 숫자 부여
-        lottoTicket.makeLottoTicket();
-        List<Integer> number = lottoTicket.getLottoNumbers();
-        //Then:오름차순으로 정렬되어 있음.
-        for (int i = 0; i < 5; i++) {
-            assertThat(number.get(i)).isLessThan(number.get(i + 1));
-        }
+        //given: 역순의 숫자 6개가 주어짐
+        List<Integer> number = List.of(6, 5, 4, 3, 2, 1);
+        //when:lottoticket을 생성할때
+        LottoTicket lottoTicket = new LottoTicket(number);
+        List<Integer> result = List.of(1, 2, 3, 4, 5, 6);
+        //then:오름차순으로 정렬해야한다.
+        Assertions.assertEquals(lottoTicket.getLottoNumbers(), result);
+    }
+
+    @Test
+    @DisplayName("일치하는 개수가 제대로 출력되는지")
+    public void WinningNumberMatchingTest() {
+        //given: 1,2,3,4,5,6 숫자를 가진 로또 1장 생성
+        List<Integer> number = List.of(1, 2, 3, 4, 5, 6);
+        LottoTicket lottoTicket = new LottoTicket(number);
+        //when:winingnumber가 1개 일치할때와 여러개 일치할때를 가정
+        List<Integer> OneWinningNumber = List.of(1, 7, 8, 9, 10, 11);
+        List<Integer> ThreeWinningNumber = List.of(1, 2, 3, 8, 9, 10);
+        //then:1개와 여러개 동일한 것을 잡아낼 수 있는가.
+        Assertions.assertEquals(1, lottoTicket.calculateMatch(OneWinningNumber));
+        Assertions.assertEquals(3, lottoTicket.calculateMatch(ThreeWinningNumber));
+    }
+
+    @Test
+    @DisplayName("보너스 볼 유무를 제대로 검출하는지")
+    public void BonusBallTest() {
+        //given: 1,2,3,4,5,6 숫자를 가진 로또 1장 생성
+        List<Integer> number = List.of(1, 2, 3, 4, 5, 6);
+        LottoTicket lottoTicket = new LottoTicket(number);
+        //when:BonusBall이 1일때(로또에 존재) 8일때(로또에 존재하지 않음.)
+        int TrueBonusBall = 1;
+        int FalseBonusBall = 8;
+        //Then:각각 존재할때 존재하지 않을때를 비교함
+        Assertions.assertTrue(lottoTicket.hasBonusBall(TrueBonusBall));
+        Assertions.assertFalse(lottoTicket.hasBonusBall(FalseBonusBall));
     }
 }
