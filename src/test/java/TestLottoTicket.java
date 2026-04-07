c21fc8ef5ef12b59637488e4ca0efad19b3d6a7e
chaehyunL <chaehyun1012@gmail.com>
2026-04-07 20:59:38 +0900

    test: test 파일 재생성

--- src/test/java/TestLottoTicket.java
+++ src/test/java/TestLottoTicket.java
@@ -1,4 +1,5 @@
 import model.LottoTicket;
+import model.LottoTickets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,25 +9,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestLottoTicket {
-    @Test
-    @DisplayName("6개의 숫자가 출력되는가")
-    public void get6numbers() {
-        //Given: 로또 티켓 한개 생성
-        LottoTicket lottoTicket = new LottoTicket();
-        //When:로또 한개를 만들고 숫자를 부여
-        lottoTicket.makeLottoTicket();
-        List<Integer> ticket = lottoTicket.getLottoNumbers();
-        //Then:로또 1장당 6개의 숫자를 가짐
-        assertEquals(ticket.size(), 6);
-    }
 
     @Test
     @DisplayName("복권이 오름차순으로 정렬되어있는가")
     public void getnumberup() {
         //Given:로또 티켓 생성
-        LottoTicket lottoTicket = new LottoTicket();
+        LottoTicket lottoTicket = new LottoTicket(List.of(6,5,4,3,2,1));
         //When:로또 티켓 숫자 부여
-        lottoTicket.makeLottoTicket();
+        List<Integer> result=List.of(1,2,3,4,5,6);
         List<Integer> number = lottoTicket.getLottoNumbers();
         //Then:오름차순으로 정렬되어 있음.
         for (int i = 0; i < 5; i++) {
--- src/test/java/TestMoney.java
+++ src/test/java/TestMoney.java
@@ -1,36 +1,61 @@
 import model.LottoRank;
+import model.LottoTickets;
 import model.Money;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class TestMoney {
     @Test
-    @DisplayName("금액->갯수 치환이 제대로 되는지")
-    public void moneyToquantity() {
-        //given:14000원입력시
-        Money money = new Money(14000);
-        //then:14개를 반환하게 된
-        assertEquals(money.getAmount(), 14);
+    @DisplayName("가격을 입력받았을때 올바른 자동티켓/수동티켓을 반환하는지")
+    public void getAmountTest() {
+        //given:2만월을 받고, 2개의 수동 티켓을 받음
+        int inputMoney = 20000;
+        Money money = new Money(inputMoney, 2);
+        //when: 2개의 수동 티켓/18개의 자동티켓/총티켓 20
+        int TrueManualamount = 2;
+        int TrueAutoamount = 18;
+        int TrueTotalAmount = 20;
+        //then:수동티켓/자동티켓/총 티켓 갯수 일치하는지 확인해주기
+        Assertions.assertEquals(TrueManualamount, money.getManualAmount());
+        Assertions.assertEquals(TrueAutoamount, money.getAutoAmount());
+        Assertions.assertEquals(TrueTotalAmount, money.getAmount());
     }
 
     @Test
-    @DisplayName("올바른 수익 반환")
-    public void testprofit() {
-        //given:돈과 status 객체 생성
-        Money money = new Money(14000);
+    @DisplayName("수익률 계산이 올바른지 테스트")
+    public void calculateRateOfReturnTest(){
+        //given 받는 돈이 20000원,그중 수익이 10000원이라고 하자
+        Money money=new Money(20000,4);
+        int profit=10000;
+        //when:money에 있는 수익률 계산 메서드를 이용할때
+        double ror=money.calculateRateOfReturn(profit);
+        //then:10000/20000과 동일한지에 대한 검증을 함.
+        Assertions.assertEquals(0.5,ror);
+    }
+
+    @Test
+    @DisplayName("여러 등수가 제대로 환산되는지")
+    public void getWinningAmountTest() {
+        //given:등수별로 하나씩 다 주어진다고 가정했을대
+        Money money = new Money(20000, 1);
         Map<LottoRank, Integer> status = new HashMap<>();
-        //when:
+        //when: 모든 status에 put해준다
         status.put(LottoRank.THREE, 1);
         status.put(LottoRank.FOUR, 1);
         status.put(LottoRank.FIVE, 1);
         status.put(LottoRank.SIX, 1);
-        int profit = (money.getProfit(status));
-        //then:가격은 합해서 2001555000
-        assertEquals(2001555000,profit);
+        //then: 결과 값과 내가 예측한 값이 동일해야한다.
+        int result = money.getWinningAmount(status);
+        int expect = LottoRank.THREE.getWinningMoney() * 1 +
+                LottoRank.FOUR.getWinningMoney() * 1 +
+                LottoRank.FIVE.getWinningMoney() * 1 +
+                LottoRank.SIX.getWinningMoney() * 1;
+
+        Assertions.assertEquals(result,expect);
     }
+
 }
