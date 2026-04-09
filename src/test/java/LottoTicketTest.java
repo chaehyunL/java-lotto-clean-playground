eb692c759feaaab0f15fb07bf58117383ce31bb4
chaehyunL <chaehyun1012@gmail.com>
2026-04-09 23:14:01 +0900

    test: Valid 유효 검증 true or false 검증로직 생성

--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -1,4 +1,5 @@
 import model.LottoTicket;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +21,55 @@ public class LottoTicketTest {
         for (int i = 0; i < 5; i++) {
             assertThat(number.get(i)).isLessThan(number.get(i + 1));
         }
+    }
+
+    @Test
+    @DisplayName("로또 넘버를 정확하게 6개 갖고 있는가")
+    public void validateSize_success( ){
+        //Given:1~6숫자 하나씩 갖고 있는 리스트
+        List<Integer> numbers=List.of(1,2,3,4,5,6);
+        //when: 로또티켓 생성
+        LottoTicket lottoTicket=new LottoTicket(numbers);
+        //theb: 유효 검증 테스트 하기
+        Assertions.assertDoesNotThrow(()->lottoTicket.vaildateSize(numbers));
+    }
+
+    @Test
+    @DisplayName("로또 넘버를 정확하게 6개 갖고 있지 않을때")
+    public void validateSize_fail( ){
+        //Given:1~6숫자 하나씩 갖고 있는 리스트
+        List<Integer> numbers=List.of(1,2,3,4,5);
+        //when: 로또티켓 생성
+
+        //then: 유효 검증 테스트 하기
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new LottoTicket(numbers).vaildateSize(numbers);
+        });
+    }
+
+    @Test
+    @DisplayName("로또 범위가 1~45 사이여야 한다")
+    public void validateRange_success( ){
+        //Given:1~6숫자 하나씩 갖고 있는 리스트
+        List<Integer> numbers=List.of(1,2,3,4,5,6);
+        //when: 로또티켓 생성
+
+        //then: 유효 검증 테스트 하기
+        Assertions.assertDoesNotThrow(() -> {
+            new LottoTicket(numbers).validateRange(numbers);
+        });
+    }
+
+    @Test
+    @DisplayName("로또 범위가 1~45 사이여야 한다-실패 ver")
+    public void validateRange_fail( ){
+        //Given:1~6숫자 하나씩 갖고 있는 리스트
+        List<Integer> numbers=List.of(0,2,3,4,5,6);
+        //when: 로또티켓 생성
+
+        //then: 유효 검증 테스트 하기
+        Assertions.assertThrows(IllegalArgumentException.class,() -> {
+            new LottoTicket(numbers).validateRange(numbers);
+        });
     }
 }
